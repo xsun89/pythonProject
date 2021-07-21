bcb1d12da76cd66f066af0094267efea8af50ad9
xsun89 <sunxin89@gmail.com>
2021-07-21 14:36:16 -0700

    0720

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 import pandas as pd
 import glob
 import os.path
+import requests
+import json
+import re
 
-def printKBs(df):
+def getKBInfo(url):
+    urlList = url.split('=')
+    lastStr = urlList[-1].rstrip("\n")
+    kbNumber = lastStr[-7:]
+    page = requests.get("https://support.microsoft.com/app/content/api/content/help/en-us/" + kbNumber)
+    print(page.content)
+    site_json = json.loads(page.content)
+    return re.sub('\W+', ' ', str(site_json["details"]["heading"]))
+
+def printKBs(df, file_object):
     for val in df['KB'].unique():
         df_kb = df.query(f'KB == @val')
-        print("-" * 30 + "KB for installation" + "-" * 30 + "\n")
-        print(df_kb[['Hostname', 'KB', 'CVE Description']].to_string(index=False), "\n")
+        kbInfo = getKBInfo(df_kb["Remediation Links"].to_string(index=False))
+        file_object.write("<h2>" + kbInfo + " for installation</h2>" + '\n')
+        _html = df_kb[['Hostname', 'KB', 'CVE Description', 'Remediation Links']].to_html(index=False)
+        file_object.write(_html)
+
 def readReportFile():
-    folder_path = r'/Users/xsun/Downloads'
+    folder_path = r'.'
     file_type = '/*csv'
     files = glob.glob(folder_path + file_type)
 
@@ -21,14 +36,16 @@ def pandasExe():
     df = pd.read_csv(reportFile)
     df = df[~df["Hostname"].isin(['RISE-PRPDEVSQL', 'RISE-RPTPRESQL', 'RISE-EXRDEVSQL'])]
     df[['Install', 'KB']] = df['Remediation Details'].str.split(r':', expand=True)
-    #print(df.info(), "\n")
-
+    file_object = open("data.html", "w")
     df_critical = df.query('Severity == "CRITICAL"')
     df_high = df.query('Severity == "HIGH"')
-    print("#" * 30 + " Critical Vulnerability " + "#" * 30 + "\n")
-    printKBs(df_critical)
-    print("#" * 30 + " High Vulnerability " + "#" * 30 + "\n")
-    printKBs(df_high)
+
+    file_object.write("<h1>Critical Vulnerability</h1>" + '\n')
+    printKBs(df_critical, file_object)
+    file_object.write("<h1>High Vulnerability</h1>" + '\n')
+    printKBs(df_high, file_object)
+    file_object.close()
+
 
 if __name__ == '__main__':
     pandasExe()
